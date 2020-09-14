60771404083d04b425b98e87c307bb962aed1eaf
Thomas Chui <thomas@ikithomas.com>
2020-09-14 16:30:59 +0800

    refactoring

---
+++ examples/conditionals.jsonnet
@@ -0,0 +1,27 @@
+local Mojito(virgin=false, large=false) = {
+  local factor = if large then 2 else 1,
+  ingredeitns: [
+    {
+      kind: 'Mint',
+      action: 'muddle',
+      qty: 6 * factor,
+      unit: 'leaves',
+    }
+  ] + (
+    if virgin then [] else [
+      { kind: 'Banks', qty: 1.5 * factor },
+    ]
+  ) + [
+    { kind: 'Lime', qty: 0.5 * factor },
+    { kind: 'Simple Syrup', qty: 0.5 * factor },
+    { kind: 'Soda', qty: 3 * factor },
+  ],
+  garnish: if large then 'Lime wedge',
+  served: 'Over crushed ice',
+};
+
+{
+  Mojito: Mojito(),
+  'Virgin Mojito': Mojito(virgin=true),
+  'Large Mojito': Mojito(large=true),
+}
--- main_test.py
+++ main_test.py
@@ -9,76 +9,132 @@ def to_json(jsonnet_path):
         stdout=subprocess.PIPE,
         check=True,
         universal_newlines=True).stdout
-    print(json_str)
+    # print(json_str)
     return json.loads(json_str)
 
 
-class TestMain():
-    def test_syntax(self):
+class TestSyntax():
+    @pytest.fixture
+    def subject(self):
         j = to_json('examples/syntax.jsonnet')
         dry_manahttan = j['cocktails']['Dry Manhattan']
+        return dry_manahttan
 
-        assert len(dry_manahttan['ingredients']) == 1
-        assert dry_manahttan['ingredients'][0]['kind'] == 'Dry Red Vermouth'
-        assert dry_manahttan['ingredients'][0]['qty'] == 1
-        assert dry_manahttan['garnish'] == 'Lemon Slice'
+    def test_ingredients(self, subject):
+        assert len(subject['ingredients']) == 1
+        assert subject['ingredients'][0]['kind'] == 'Dry Red Vermouth'
+        assert subject['ingredients'][0]['qty'] == 1
 
-    def test_variables(self):
+    def test_garnish(self, subject):
+        assert subject['garnish'] == 'Lemon Slice'
+
+
+class TestVariables():
+    @pytest.fixture
+    def subject(self):
         j = to_json('examples/variables.jsonnet')
-        mojito = j['Mojito']
+        return j['Mojito']
 
-        assert len(mojito['ingredients']) == 5
-        assert mojito['ingredients'][3]['kind'] == 'Simple Syrup'
-        assert mojito['ingredients'][3]['qty'] == 0.5
+    def test_ingredients(self, subject):
+        assert len(subject['ingredients']) == 5
+        assert subject['ingredients'][3]['kind'] == 'Simple Syrup'
+        assert subject['ingredients'][3]['qty'] == 0.5
 
-    def test_references(self):
+
+class TestReferences():
+    @pytest.fixture
+    def subject(self):
         j = to_json('examples/references.jsonnet')
-        fizz = j['Gin Fizz']
+        return j['Gin Fizz']
 
-        assert fizz['served'] == 'Tall'
+    def test_served(self, subject):
+        assert subject['served'] == 'Tall'
 
-    def test_inner_reference(self):
-        j = to_json('examples/inner-reference.jsonnet')
 
-        assert j['Martini']['ingredients'][1]['qty'] == 1
+class TestInnerReference():
+    @pytest.fixture
+    def subject(self):
+        return to_json('examples/inner-reference.jsonnet')
 
-    def test_arithmetic(self):
-        j = to_json('examples/arithmetic.jsonnet')
+    def test_subject(self, subject):
+        assert subject['Martini']['ingredients'][1]['qty'] == 1
 
-        assert j['concat_array'] == [1, 2, 3, 4]
-        assert j['concat_string'] == '1234'
-        assert not j['equality1']
-        assert j['equality2']
-        assert pytest.approx(j['ex1'], 0.01) == 1.66
-        assert j['ex2'] == 3
-        assert j['ex3'] == 1
-        assert j['ex4']
-        assert j['obj'] == {
+
+class TestArithmetic():
+    @pytest.fixture
+    def subject(self):
+        return to_json('examples/arithmetic.jsonnet')
+
+    def test_concat_array(self, subject):
+        assert subject['concat_array'] == [1, 2, 3, 4]
+
+    def test_concat_string(self, subject):
+        assert subject['concat_string'] == '1234'
+
+    def test_equality1(self, subject):
+        assert not subject['equality1']
+
+    def test_equality2(self, subject):
+        assert subject['equality2']
+
+    def test_ex(self, subject):
+        assert pytest.approx(subject['ex1'], 0.01) == 1.66
+        assert subject['ex2'] == 3
+        assert subject['ex3'] == 1
+        assert subject['ex4']
+
+    def test_obj(self, subject):
+        assert subject['obj'] == {
             'a': 1,
             'b': 3,
             'c': 4,
         }
-        assert j['str1'] == 'The value of self.ex2 is 3.'
-        assert j['str2'] == 'The value of self.ex2 is 3.'
-        assert j['str3'] == 'ex1=1.67, ex2=3.00'
-        assert j['str4'] == 'ex1=1.67, ex2=3.00'
-        assert j['str5'] == 'ex1=1.67\nex2=3.00\n'
 
-    def test_functions(self):
-        j = to_json('examples/functions.jsonnet')
+    def test_obj_member(self, subject):
+        assert subject['obj_member']
 
-        assert j['first_class_function'] == 25
-        assert pytest.approx(j['inline_function'], 0.01) == 79.54
-        assert pytest.approx(j['multiline_function'], 0.01) == 160.6
-        assert j['named_params'] == 6
-        assert j['call_method'] == 'Meow! foo'
-        assert j['standard_lib'] == 'foo bar'
-        assert j['len'] == [5, 3]
+    def test_str(self, subject):
+        assert subject['str1'] == 'The value of self.ex2 is 3.'
+        assert subject['str2'] == 'The value of self.ex2 is 3.'
+        assert subject['str3'] == 'ex1=1.67, ex2=3.00'
+        assert subject['str4'] == 'ex1=1.67, ex2=3.00'
+        assert subject['str5'] == 'ex1=1.67\nex2=3.00\n'
 
-    def test_sours(self):
-        j = to_json('examples/sours.jsonnet')
 
-        assert j['Pisco Sour'] == {
+class TestFunctions():
+    @pytest.fixture
+    def subject(self):
+        return to_json('examples/functions.jsonnet')
+
+    def test_first_class_function(self, subject):
+        assert subject['first_class_function'] == 25
+
+    def test_inline_function(self, subject):
+        assert pytest.approx(subject['inline_function'], 0.01) == 79.54
+
+    def test_multiline_function(self, subject):
+        assert pytest.approx(subject['multiline_function'], 0.01) == 160.6
+
+    def test_named_params(self, subject):
+        assert subject['named_params'] == 6
+
+    def test_call_method(self, subject):
+        assert subject['call_method'] == 'Meow! foo'
+
+    def test_standard_lib(self, subject):
+        assert subject['standard_lib'] == 'foo bar'
+
+    def test_len(self, subject):
+        assert subject['len'] == [5, 3]
+
+
+class TestObjectConstructor():
+    @pytest.fixture
+    def subject(self):
+        return to_json('examples/object-constructor.jsonnet')
+
+    def test_object_constructor(self, subject):
+        assert subject['Pisco Sour'] == {
             'garnish': 'Angostura bitters',
             'ingredients': [
                 {'kind': 'Machu Pisco', 'qty': 2},
@@ -87,4 +143,4 @@ class TestMain():
                 {'kind': 'Simple Syrup', 'qty': 1},
             ],
             'served': 'Straight Up'
-       }
+        }
